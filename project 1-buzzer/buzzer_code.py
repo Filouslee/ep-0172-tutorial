8d846fdf32312a29ec67eb82691149aa1b1ef425
Filouslee <filouslee@gmail.com>
2025-05-08 20:56:25 +0200

    Update buzzer_code.py

--- project 1-buzzer/buzzer_code.py
+++ project 1-buzzer/buzzer_code.py
@@ -1,49 +1,39 @@
-from machine import Pin, PWM
+from machine import ADC, PWM, Pin
 import time
- 
 
-# Initialisation du bouton (BTN1 sur GP15)
-button = Pin(15, Pin.IN, Pin.PULL_UP)
-bouton = Pin(14, Pin.IN, Pin.PULL_UP)
+# Joystick
+joy_x = ADC(26)
+joy_y = ADC(27)
 
-# Initialisation du buzzer (GP13)
+# Buzzer
 buzzer = PWM(Pin(13))
-buzzer.duty_u16(0)  # On commence éteint
-sg90 = PWM(Pin(12, mode=Pin.OUT))
-sg90.freq(50)
-min = 1638
-max = 7864
+
+# Bouton toggle (BTN2 sur GP14)
+toggle_button = Pin(14, Pin.IN, Pin.PULL_UP)
+
+# Variables d'état
+buzzer_enabled = True
+last_button_state = 1
+
 while True:
-    if not button.value():  # bouton appuyé = niveau bas
-        buzzer.freq(1000)         # fréquence de 1kHz
-        buzzer.duty_u16(2768)    # 50% de duty cycle
-        time.sleep(1)
-        buzzer.duty_u16(0)
-        time.sleep(1)
-        buzzer.freq(10000)         # fréquence de 1kHz
-        buzzer.duty_u16(32768)    # 50% de duty cycle
-        time.sleep(1)
-        buzzer.duty_u16(0)
-        time.sleep(1)
-        buzzer.freq(300000)         # fréquence de 1kHz
-        buzzer.duty_u16(62768)    # 50% de duty cycle
-        time.sleep(1)
-        buzzer.duty_u16(0)
-        time.sleep(1)
-    if not bouton.value():
-        sg90.duty_u16(min)
-        time.sleep(1)
-        sg90.duty_u16(max)
-        time.sleep(1)
-        sg90.duty_u16(min)
-        time.sleep(1)
-        sg90.duty_u16(max)
-        time.sleep(1)
-        sg90.duty_u16(min)
-        time.sleep(1)
-        sg90.duty_u16(max)
-        time.sleep(1)
+    current_state = toggle_button.value()
+
+    # Détection du front descendant (appui)
+    if last_button_state == 1 and current_state == 0:
+        buzzer_enabled = not buzzer_enabled  # Inverse l'état
+
+    last_button_state = current_state
+
+    if buzzer_enabled:
+        x_val = joy_x.read_u16()
+        y_val = joy_y.read_u16()
+
+        freq = int(200 + (x_val / 65535) * 1800)
+        duty = int((y_val / 65535) * 32768)
+
+        buzzer.freq(freq)
+        buzzer.duty_u16(duty)
     else:
-        buzzer.duty_u16(0)        # éteindre le buzzer
+        buzzer.duty_u16(0)
 
     time.sleep(0.05)
